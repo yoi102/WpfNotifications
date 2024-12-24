eda7e466620adf531c5d41d14fed6626a283c340
yoiri <yoiri102@gmail.com>
2024-12-25 00:32:13 +0900

    Notifications

--- Notifications/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Windows;
-using System.Windows.Markup;
-
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
-[assembly: XmlnsPrefix(@"https://github.com/yoi102/xaml/controls", "yr")]
---
+++ Notifications/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsPrefix(@"https://github.com/yoi102/xaml/controls", "yr")]
+[assembly: XmlnsDefinition(@"https://github.com/yoi102/xaml/controls", "Notifications.Controls")]
