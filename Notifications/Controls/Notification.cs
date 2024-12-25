153288404d03733763f7333415dd5272ae7051a1
yoiri <yoiri102@gmail.com>
2024-12-26 01:18:33 +0900

    Notifications

--- Notifications/Controls/Notification.cs
+++ Notifications/Controls/Notification.cs
@@ -1,10 +1,11 @@
-﻿using System.ComponentModel;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media.Animation;
+using System.Windows.Shapes;
 
 namespace Notifications.Controls
 {
+    [TemplatePart(Name = "PART_ColorBar", Type = typeof(Rectangle))]
     public class Notification : ContentControl
     {
         // Using a DependencyProperty as the backing store for ExpirationTime.  This enables animation, styling, binding, etc...
@@ -21,6 +22,8 @@ namespace Notifications.Controls
            "NotificationClosing", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(Notification));
 
         private TimeSpan _closingAnimationTime = TimeSpan.Zero;
+
+        private Rectangle _colorBar = null!;
 
         public event RoutedEventHandler NotificationClosed
         {
@@ -48,27 +51,6 @@ namespace Notifications.Controls
 
         public bool IsClosing { get; set; }
 
-        public virtual async Task ScheduleCloseAsync(TimeSpan expirationTime)
-        {
-            ExpirationTime = expirationTime;
-            RaiseEvent(new RoutedEventArgs(NotificationClosingEvent));
-
-            if (expirationTime == TimeSpan.MaxValue)
-            {
-                return;
-            }
-            await Task.Delay(expirationTime);
-
-            await CloseAsync();
-        }
-
-        public override void OnApplyTemplate()
-        {
-            base.OnApplyTemplate();
-            var storyboards = Template.Triggers.OfType<EventTrigger>().FirstOrDefault(t => t.RoutedEvent == NotificationCloseInvokedEvent)?.Actions.OfType<BeginStoryboard>().Select(a => a.Storyboard);
-            _closingAnimationTime = new TimeSpan(storyboards?.Max(s => Math.Min((s.Duration.HasTimeSpan ? s.Duration.TimeSpan + (s.BeginTime ?? TimeSpan.Zero) : TimeSpan.MaxValue).Ticks, s.Children.Select(ch => ch.Duration.TimeSpan + (s.BeginTime ?? TimeSpan.Zero)).Max().Ticks)) ?? 0);
-        }
-
         public async Task CloseAsync()
         {
             if (IsClosing)
@@ -80,6 +62,60 @@ namespace Notifications.Controls
             RaiseEvent(new RoutedEventArgs(NotificationCloseInvokedEvent));
             await Task.Delay(_closingAnimationTime);
             RaiseEvent(new RoutedEventArgs(NotificationClosedEvent));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            _colorBar = (Rectangle)GetTemplateChild("PART_ColorBar");
+
+            var closingStoryboards = Template.Triggers.OfType<EventTrigger>().FirstOrDefault(t => t.RoutedEvent == NotificationCloseInvokedEvent)?.Actions.OfType<BeginStoryboard>().Select(a => a.Storyboard);
+            _closingAnimationTime = new TimeSpan(closingStoryboards?.Max(s => Math.Min((s.Duration.HasTimeSpan ? s.Duration.TimeSpan + (s.BeginTime ?? TimeSpan.Zero) : TimeSpan.MaxValue).Ticks, s.Children.Select(ch => ch.Duration.TimeSpan + (s.BeginTime ?? TimeSpan.Zero)).Max().Ticks)) ?? 0);
+        }
+
+        public virtual async Task ScheduleCloseAsync(TimeSpan expirationTime)
+        {
+            ExpirationTime = expirationTime;
+            RaiseEvent(new RoutedEventArgs(NotificationClosingEvent));
+            await Task.Delay(10);
+
+            AnimateWidthToZero(expirationTime);
+
+            if (expirationTime == TimeSpan.MaxValue)
+            {
+                return;
+            }
+            await Task.Delay(expirationTime);
+
+            await CloseAsync();
+        }
+
+        private void AnimateWidthToZero(TimeSpan expirationTime)
+        {
+            if (Content is not string && Content is not NotificationContent)
+            {
+                _colorBar.Visibility = Visibility.Collapsed;
+                return;
+            }
+
+            if (_colorBar is null)
+                return;
+
+            DoubleAnimation widthAnimation = new DoubleAnimation
+            {
+                From = _colorBar.RenderSize.Width,
+                To = 0,
+                Duration = new Duration(expirationTime),
+                EasingFunction = new QuadraticEase()
+            };
+
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(widthAnimation);
+
+            Storyboard.SetTarget(widthAnimation, _colorBar);
+            Storyboard.SetTargetProperty(widthAnimation, new PropertyPath(FrameworkElement.WidthProperty));
+
+            storyboard.Begin();
         }
     }
 }
